929e5b0eef3bfd5414579d961341ac39ddfe0efc
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-10-04 19:22:26 -0600

    se crea el primer comentario de socket

---
+++ index.js
@@ -0,0 +1,43 @@
+const allowedSites = [
+  'http://localhost:9000',
+  'http://192.168.10.160:9000',
+
+];
+
+const { Server } = require('socket.io');
+
+const io = new Server({ cors: { origin: allowedSites } });
+
+io.on("connection", dep => {
+  console.log(`connect ${dep.id}`);
+  dep.on('Conexion', (params) => {
+    console.log(`Conexion a sucursal ${params.session.store.alias}`)
+    const room = params.session.store.id
+    dep.join(room)
+    dep.room = room
+    dep.emit('Room', (room))
+  })
+
+  dep.on('Create', (formulario) => {
+    console.log(formulario);
+    if (dep.room) {
+      dep.to(dep.room).emit('StoreList', formulario);
+    } else {
+      console.error('Room no está definido');
+    }
+    io.emit('List', formulario);
+  })
+  
+  dep.on('ChangeStatus', (formulario) => {
+    console.log(formulario.store.id)
+    dep.to(formulario.store.id).emit('ChangeStatus',formulario)
+  })
+
+
+  dep.on("disconnect", (reason) => {
+    console.log(`disconnect ${dep.id} due to ${reason}`);
+  });
+});
+
+
+io.listen(3000);
